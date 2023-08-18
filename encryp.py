026e4556f506c592febc0feb9263cd2a3c5fea41
yadav <yadavharshit311@gmail.com>
2023-08-18 20:24:09 +0530

    final commit

--- encryp.py
+++ encryp.py
@@ -1,1 +1,32 @@
-print("LETS GETS THIS")
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_ECB)
+
+
+def encrypt(plaintext):
+    padded_plaintext = pad(plaintext.encode(), AES.block_size)
+    ciphertext = cipher.encrypt(padded_plaintext)
+    return ciphertext
+
+
+def decrypt(ciphertext):
+    padded_plaintext = cipher.decrypt(ciphertext)
+    plaintext = unpad(padded_plaintext, AES.block_size)
+    return plaintext.decode()
+
+
+plaintext = input("enter the text")
+print("Plaintext:", plaintext)
+encrypted_text = encrypt(plaintext)
+print("Encrypted text:", encrypted_text)
+x=input("do you want to decrypt...y/n? ")
+if x=='y' or x=='Y':
+    decrypted_text = decrypt(encrypted_text)
+    print("Decrypted text:", decrypted_text)
+else:
+    print("end")
+
+
